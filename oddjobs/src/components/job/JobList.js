411eed8eb0b702407d2833a0fc7ac82793d36c11
corneliajohnson <corneliaj91@gmail.com>
2020-12-23 15:25:20 -0500

    added job.css

--- oddjobs/src/components/job/JobList.js
+++ oddjobs/src/components/job/JobList.js
@@ -6,7 +6,7 @@ import { useHistory } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { DeleteJob } from "./DeleteJob";
-import { Redirect } from "react-router-dom";
+import "./Job.css";
 
 export const JobList = () => {
   const { jobs, getJobs } = useContext(JobContext);
@@ -26,44 +26,48 @@ export const JobList = () => {
   }, [jobs]);
 
   return (
-    <div className="jobs">
-      <h2>Open Jobs</h2>
-      <button
-        onClick={() => {
-          if (currentUser) {
-            history.push("/jobs/create");
-          } else {
-            history.push("/login");
-          }
-        }}
-      >
-        Add Job
-      </button>
-      <table className="u-full-width">
-        <thead>
-          <tr>
-            <th>Job</th>
-            <th>Category</th>
-            <th>Pay</th>
-            <th>Location</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {visibleJobs.map((job) => (
-            <Job
-              key={job.id}
-              job={job}
-              editBtn={
-                <Link to={`/jobs/edit/${job.id}`}>
-                  <FontAwesomeIcon icon={faEdit} />
-                </Link>
-              }
-              deleteBtn={<DeleteJob id={job.id} title={job.title} />}
-            />
-          ))}
-        </tbody>
-      </table>
+    <div className="container">
+      <div className="jobs-header">
+        <h2>Open Jobs</h2>
+        <button
+          onClick={() => {
+            if (currentUser) {
+              history.push("/jobs/create");
+            } else {
+              history.push("/login");
+            }
+          }}
+        >
+          Add Job
+        </button>
+      </div>
+      <div className="jobs">
+        <table className="u-full-width">
+          <thead>
+            <tr>
+              <th>Job</th>
+              <th>Category</th>
+              <th>Pay</th>
+              <th>Location</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {visibleJobs.map((job) => (
+              <Job
+                key={job.id}
+                job={job}
+                editBtn={
+                  <Link to={`/jobs/edit/${job.id}`}>
+                    <FontAwesomeIcon icon={faEdit} />
+                  </Link>
+                }
+                deleteBtn={<DeleteJob id={job.id} title={job.title} />}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
