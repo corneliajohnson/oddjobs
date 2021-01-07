1780fb03b3c2a780ca505e62e2f7f5362b4b90a0
corneliajohnson <corneliaj91@gmail.com>
2021-01-06 19:39:57 -0500

    added user info to account page

--- oddjobs/src/components/user/UserAccount.js
+++ oddjobs/src/components/user/UserAccount.js
@@ -1,5 +1,40 @@
 import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "./UserProvider";
 
 export const UserAccount = () => {
-  return <h1 className="mt-5">userAccount</h1>;
+  const { getUserById } = useContext(UserContext);
+  const [user, setUser] = useState({});
+
+  const currentUser = localStorage.getItem("user");
+
+  useEffect(() => {
+    getUserById(currentUser).then((response) => {
+      setUser(response);
+    });
+  }, []);
+
+  return (
+    <div className="container" style={{ marginTop: "7em" }}>
+      <div className="row">
+        <div className="one-half column">Image</div>
+        <div className="one-half column">
+          <div className="card">
+            <div className="card-body">
+              <div className="card-text">
+                {user.firstName} {user.lastName}
+              </div>
+              <div className="card-text">{user.email}</div>
+              <div className="card-text">{user.phone}</div>
+              <div>Number of Post</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div>
+          <h3>My Post</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
